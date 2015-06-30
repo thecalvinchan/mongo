6d164726569dbc86a10aba53aef54fa193084b6a
Margaret Stephenson <margaretstephenson@gmail.com>
2015-06-30 10:39:31 -0400

    started parser:

--- src/mongo/scripting/tinyjs/ast_parser.cpp
+++ src/mongo/scripting/tinyjs/ast_parser.cpp
@@ -3,6 +3,7 @@
 #include "mongo/scripting/tinyjs/assignment_operator.h"
 #include "mongo/scripting/tinyjs/arithmetic_operator.h"
 #include "mongo/scripting/tinyjs/comparison_operator.h"
+#include "mongo/scripting/tinyjs/lexer.h"
 #include "mongo/scripting/tinyjs/logical_operator.h"
 #include "mongo/scripting/tinyjs/unary_operator.h"
 #include "mongo/scripting/tinyjs/operand.h"
@@ -10,43 +11,35 @@
 #include <stack>
 
 namespace mongo {
+namespace tinyjs {
 
-Node* parseTokens(std::vector<Token>::iterator it,
-                  std::vector<Token>::iterator end,
-                  TokenType stopToken = null);
+Token currentToken;
 
 ASTParser::ASTParser(std::vector<Token> tokens) {
     Node* head = parseTokens(tokens.begin(), tokens.end());
 }
 
-bool checkMatch (std::vector<Token> tokens, NodeType startProduction) {
-    std::vector<Token>::iterator ip tokenPointer = tokens.begin();
-    std::stack<ProductionType> productionStack;
-    productionStack.push_back(CLAUSE);
-    while (!productionStack.empty()) {
-        ProductionType nextProd = productionStack.top();
-        if (nextProd == ip->type) {
-            productionStack.pop();
-            ip++;
-        } else if (nextProd == kOptional) {
-            productionStack.pop();
-        } else if (isTerminal(nextProd)) { 
-            // TODO: throw exception
-        } else {
-            ProductionType productions[] = ProductionTable.at(nextProd).at(ip->type);
-            // throws out_of_range exception if not found
-            // this means no entry in production table
-            // TODO: build tree and node
-            productionStack.pop();
-            for (int i = productions.size()-1; i>=0; i--) {
-                productionStack.push(productions[i]);
-            }
-        }
+void nexttoken(void);
+void error(const char msg[]);
+
+int accept(Token t) {
+    if (currentToken == t) {
+        nexttoken();
+        return 1;
     }
+    return 0;
 }
 
-bool isTerminal(ProductionType production) {
-    if (production < 38) {
-        return true;
-    }
+int expect(Token t) {
+    if (accept(t))
+        return 1;
+    error("expect: unexpected token");
+    return 0;
+}
+
+void clauseAction() {
+    
+}
+
+}
 }
