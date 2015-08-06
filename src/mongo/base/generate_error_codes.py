420813af049c3dfc699b2ea05da9d658903247c9
Calvin Chan <calvin.chan.h@gmail.com>
2015-08-06 11:25:35 -0400

    Moved everything to one file

--- src/mongo/base/generate_error_codes.py
+++ src/mongo/base/generate_error_codes.py
@@ -40,7 +40,7 @@ Usage:
 """
 
 from optparse import OptionParser
-import sys
+import sys, yaml
 import err_generators.cpp as cpp
 
 def main(options, args):
@@ -48,10 +48,15 @@ def main(options, args):
         usage('Wrong number of arguments')
     error_codes, error_classes = parse_error_definitions_from_file(args[0])
     check_for_conflicts(error_codes, error_classes)
-    if (options.cpp):
-        cpp_generator = cpp.Generator(error_codes, error_classes, options.cpp)
-        cpp_generator.generate()
-    elif (options.js):
+    generator_options = yaml.load(options.generator_options)
+    print generator_options
+    if (options.generator == 'cpp'):
+        if (generator_options['cpp_header'] and generator_options['cpp_source']):
+            cpp_generator = CPP_Generator(error_codes, error_classes, generator_options)
+            cpp_generator.generate()
+        else:
+            usage('Must specify CPP header and source files')
+    elif (options.generator == 'js'):
         print "Use JS generator"
     else:
         usage('Must specify which generator(s) to use.')
@@ -126,10 +131,198 @@ def has_missing_error_codes(error_codes, error_classes):
                 failed = True
     return failed
 
+class Base_Generator:
+    def __init__(self, error_codes, error_classes, options):
+        self.options = options
+        self.error_codes = error_codes
+        self.error_classes = error_classes
+    def generate():
+        return 0
+
+class CPP_Generator(Base_Generator):
+    def generate(self):
+        self.generate_header()
+        self.generate_source()
+
+    def generate_header(self):
+
+        enum_declarations = ',\n            '.join('%s = %s' % ec for ec in self.error_codes)
+        predicate_declarations = ';\n        '.join(
+            'static bool is%s(Error err)' % ec[0] for ec in self.error_classes)
+
+        open(self.options['cpp_header'], 'wb').write(self.header_template % dict(
+                error_code_enum_declarations=enum_declarations,
+                error_code_class_predicate_declarations=predicate_declarations))
+
+    def generate_source(self):
+        symbol_to_string_cases = ';\n        '.join(
+            'case %s: return "%s"' % (ec[0], ec[0]) for ec in self.error_codes)
+        string_to_symbol_cases = ';\n        '.join(
+            'if (name == "%s") return %s' % (ec[0], ec[0])
+            for ec in self.error_codes)
+        int_to_symbol_cases = ';\n        '.join(
+            'case %s: return %s' % (ec[0], ec[0]) for ec in self.error_codes)
+        predicate_definitions = '\n    '.join(
+            self.generate_error_class_predicate_definition(*ec) for ec in self.error_classes)
+        open(self.options['cpp_source'], 'wb').write(self.source_template % dict(
+                symbol_to_string_cases=symbol_to_string_cases,
+                string_to_symbol_cases=string_to_symbol_cases,
+                int_to_symbol_cases=int_to_symbol_cases,
+                error_code_class_predicate_definitions=predicate_definitions))
+
+    def generate_error_class_predicate_definition(self, class_name, code_names):
+        cases = '\n        '.join('case %s:' % c for c in code_names)
+        return self.error_class_predicate_template % dict(class_name=class_name, cases=cases)
+
+    header_template = '''// AUTO-GENERATED FILE DO NOT EDIT
+    // See src/mongo/base/generate_error_codes.py
+    /*    Copyright 2014 MongoDB, Inc.
+     *
+     *    This program is free software: you can redistribute it and/or  modify
+     *    it under the terms of the GNU Affero General Public License, version 3,
+     *    as published by the Free Software Foundation.
+     *
+     *    This program is distributed in the hope that it will be useful,
+     *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+     *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     *    GNU Affero General Public License for more details.
+     *
+     *    You should have received a copy of the GNU Affero General Public License
+     *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+     *
+     *    As a special exception, the copyright holders give permission to link the
+     *    code of portions of this program with the OpenSSL library under certain
+     *    conditions as described in each individual source file and distribute
+     *    linked combinations including the program with the OpenSSL library. You
+     *    must comply with the GNU Affero General Public License in all respects
+     *    for all of the code used other than as permitted herein. If you modify
+     *    file(s) with this exception, you may extend this exception to your
+     *    version of the file(s), but you are not obligated to do so. If you do not
+     *    wish to do so, delete this exception statement from your version. If you
+     *    delete this exception statement from all source files in the program,
+     *    then also delete it in the license file.
+     */
+
+#pragma once
+
+#include <string>
+
+#include "mongo/base/string_data.h"
+
+    namespace mongo {
+
+        /**
+         * This is a generated class containing a table of error codes and their corresponding error
+         * strings. The class is derived from the definitions in src/mongo/base/error_codes.err file.
+         *
+         * Do not update this file directly. Update src/mongo/base/error_codes.err instead.
+         */
+
+        class ErrorCodes {
+        public:
+            enum Error {
+                %(error_code_enum_declarations)s,
+                MaxError
+            };
+
+            static std::string errorString(Error err);
+
+            /**
+             * Parses an Error from its "name".  Returns UnknownError if "name" is unrecognized.
+             *
+             * NOTE: Also returns UnknownError for the string "UnknownError".
+             */
+            static Error fromString(StringData name);
+
+            /**
+             * Casts an integer "code" to an Error.  Unrecognized codes are preserved, meaning
+             * that the result of a call to fromInt() may not be one of the values in the
+             * Error enumeration.
+             */
+            static Error fromInt(int code);
+
+            %(error_code_class_predicate_declarations)s;
+        };
+
+    }  // namespace mongo
+    '''
+
+    source_template = '''// AUTO-GENERATED FILE DO NOT EDIT
+    // See src/mongo/base/generate_error_codes.py
+    /*    Copyright 2014 MongoDB, Inc.
+     *
+     *    This program is free software: you can redistribute it and/or  modify
+     *    it under the terms of the GNU Affero General Public License, version 3,
+     *    as published by the Free Software Foundation.
+     *
+     *    This program is distributed in the hope that it will be useful,
+     *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+     *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     *    GNU Affero General Public License for more details.
+     *
+     *    You should have received a copy of the GNU Affero General Public License
+     *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+     *
+     *    As a special exception, the copyright holders give permission to link the
+     *    code of portions of this program with the OpenSSL library under certain
+     *    conditions as described in each individual source file and distribute
+     *    linked combinations including the program with the OpenSSL library. You
+     *    must comply with the GNU Affero General Public License in all respects
+     *    for all of the code used other than as permitted herein. If you modify
+     *    file(s) with this exception, you may extend this exception to your
+     *    version of the file(s), but you are not obligated to do so. If you do not
+     *    wish to do so, delete this exception statement from your version. If you
+     *    delete this exception statement from all source files in the program,
+     *    then also delete it in the license file.
+     */
+
+#include "mongo/base/error_codes.h"
+
+
+#include "mongo/util/mongoutils/str.h"
+
+    namespace mongo {
+
+        std::string ErrorCodes::errorString(Error err) {
+            switch (err) {
+            %(symbol_to_string_cases)s;
+            default: return mongoutils::str::stream() << "Location" << err;
+            }
+        }
+
+        ErrorCodes::Error ErrorCodes::fromString(StringData name) {
+            %(string_to_symbol_cases)s;
+            return UnknownError;
+        }
+
+        ErrorCodes::Error ErrorCodes::fromInt(int code) {
+            return static_cast<Error>(code);
+        }
+
+        %(error_code_class_predicate_definitions)s
+
+    namespace {
+        static_assert(sizeof(ErrorCodes::Error) == sizeof(int), "sizeof(ErrorCodes::Error) == sizeof(int)");
+    }  // namespace
+    }  // namespace mongo
+    '''
+
+    error_class_predicate_template = '''bool ErrorCodes::is%(class_name)s(Error err) {
+            switch (err) {
+            %(cases)s
+                return true;
+            default:
+                return false;
+            }
+        }
+    '''
+
 if __name__ == '__main__':
     usage_msg = "usage: %prog [options] /path/to/error_codes.err"
     parser = OptionParser(usage=usage_msg)
     parser.add_option('--cpp', dest='cpp', nargs=2, metavar=('PATH_TO_CPP_HEADER','PATH_TO_CPP_SOURCE'), help='use cpp generator. Must also provide two arguments to location of generated header and source.')
     parser.add_option('--js', dest='js', nargs=1, metavar='PATH_TO_JS_FILE', help='user js generator. Must also provide an argument to location of generated js file')
+    parser.add_option('--generator', dest='generator', nargs=1, metavar='GENERATOR', type='choice', choices=['cpp','js'], help='specify generator to use')
+    parser.add_option('--generator_options', dest='generator_options', nargs=1, metavar='GENERATOR_OPTIONS', help='specify generator options')
     (options, args) = parser.parse_args()
     main(options, args)
