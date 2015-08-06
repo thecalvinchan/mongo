ff1ba57f6f070c47aed81921553f4323af26896a
Calvin Chan <calvin.chan.h@gmail.com>
2015-08-06 11:25:35 -0400

    Moved everything to one file

--- src/mongo/base/err_generators/base.py
+++
@@ -1,7 +0,0 @@
-class Generator:
-    def __init__(self, error_codes, error_classes, args):
-        self.args = args
-        self.error_codes = error_codes
-        self.error_classes = error_classes
-    def generate():
-        print "no-op"
